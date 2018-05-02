1017950ac16025eebadb5bae2b68404108815b22
Felix Khafizov N <asssama212@gmail.com>
2018-05-02 18:33:00 +0500

    Made method for getting input fields data

--- app.js
+++ app.js
@@ -1,20 +1,64 @@
+//Ui block
+
+var UIController = (function(){
+    //Private block!
+
+    //Define an object with css classes of input elements
+
+    var DOMStrings = {
+        inputDescription: ".add__description",
+        inputType: ".add__type",
+        inputValue: ".add__value",
+        inputAdd: ".add__btn"
+    }
+
+    //Public block - that will be returned after IIFE execution
+
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value,
+            }
+        },
+
+        getDOMStrings: function(){
+            return DOMStrings;
+        }
+    }
+
+
+})();
+
+
+//Data block
+var budgetController = (function(){
+
+})();
+
 var controller = (function(budgetCtrl, UICtrl){
 
     var ctrlAddItem = function() {
 
-        //TODO: 1. Get the input field data
+    //TODO: 1. Get the input field data
+
+    input = UICtrl.getInput();
+    console.log(input);
+
+    //TODO: 2. Add the item to the budget controller
 
 
+    //TODO: 3. Add the item to the UI
 
-        //TODO: 2. Add the item to the budget controller
+    //TODO: 4. Calculate the budhet
 
-        //TODO: 3. Add the item to the UI
+    //TODO: 5. Display the budget on the UI
 
-        //TODO: 4. Calculate the budhet
-
-        //TODO: 5. Display the budget on the UI
 
     }
+
+    //Add event listeners on clicking enter and add button
 
     document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
 
@@ -25,13 +69,3 @@ var controller = (function(budgetCtrl, UICtrl){
     });
 
 })(budgetController, UIController);
-
-
-var UIController = (function(){
-
-})();
-
-
-var budgetController = (function(){
-
-})();
